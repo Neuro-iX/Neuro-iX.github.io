93cafb7bead00c82f4e0920d730dc434eb8a1ec8
bverreman <benverreman@gmail.com>
2023-08-30 18:13:27 -0400

    add pmid, pmcid and doi to each post

--- pre_render/posts_generator.py
+++ pre_render/posts_generator.py
@@ -115,6 +115,7 @@ class Article:
         self.toFindKeywords()
         self.toFindDoi()
         self.toFindPmid()
+        self.toFindPmcid()
         
     # Soup    
     def toGetSoup(self, *args):
@@ -284,8 +285,20 @@ class Article:
     def toGetPmid(self, *args):
         return self.pmid 
 
+    # PMCID
+    def toFindPmcid(self, *args):
+        try:
+            self.pmcid = self.soup.find('PMCID').text
+        except AttributeError:
+            self.pmcid = ""
+            pass
+        
+    def toGetPmcid(self, *args):
+        return self.pmcid 
+      
+    # Publishing
     def toPublish(self, *args):
-        title_part ='_'.join(self.title.split()[:6]) # take the first 6 words and replace whitespace by underscore
+        title_part = '_'.join(self.title.split()[:6]) # take the first 6 words and replace whitespace by underscore
         title_part = re.sub('[^a-zA-Z0-9s\\_]', '', title_part) # remove special characters except underscore
         title_part = title_part.lower() 
         
@@ -295,9 +308,14 @@ class Article:
         if not os.path.exists(path):
             os.makedirs(path)
         
+        if self.pmcid: #if self.pmcid is not empty
+            identification = "PMID: " + self.pmid + "    PMCID: " + self.pmcid + "    DOI: [" + self.doi + "](https://doi.org/" + self.doi + ")" #{target=" + '\"' + "_blank" + '\"' + "}"
+        else:
+            identification = "PMID: " + self.pmid + "    DOI: [" + self.doi + "](https://doi.org/" + self.doi + ")" #{target=" + '"' + "_blank" + '"' + "}"
+        
         new_post="""---
 title: {title}
-#description: {description}
+description: {description}
 author:
 {authors}
 date: {date}
@@ -310,7 +328,7 @@ engine: knitr
 ---
 
 {abstract}
-        """.format(title = '"'+unidecode(self.title)+'"', description = '"'+unidecode(self.affiliations)+'"', authors = unidecode(self.authors), date = '"'+unidecode(self.date)+'"', categories = self.keywords, abstract = unidecode(self.abstract))
+        """.format(title = '"'+unidecode(self.title)+'"', description = '"'+unidecode(identification)+'"', authors = unidecode(self.authors), date = '"'+unidecode(self.date)+'"', categories = self.keywords, abstract = unidecode(self.abstract))
         
         with open(path+"/index.qmd", 'w') as f:
           f.write(new_post)        
@@ -571,8 +589,269 @@ class ArticlesFinder:
                     """
         except AttributeError:
             pass
-#----------------------
           
+          
+#----------------------
+
+
+
+
+class ArticlesPublisher:
+    """
+    A class used to represent a GUI to create the posts of the articles previously selected
+
+    ...
+
+    Attributes
+    ----------
+    maxi : str
+        Maximum number of articles to search for
+    pmid : str
+        List of PMID of articles on Pubmed
+    days : str
+        Maximum number of days since publishing date
+    terms : str
+        Key terms (for example: Bouix[Author])
+    results : str
+        List of the title, date, first author and DOI of all the articles found
+        
+    Methods
+    -------
+    search(self, *args) -> (str) we, (str) qk
+        Creates the command for Entrez API and gets WebEnv and query_key
+    fetch(self, *args) -> (str) results
+        Creates the command for Entrez API and gets searching data results
+    """
+    
+    
+    # Modules
+    def __init__(self, root):
+        """Initialize a root window with every buttons and entries
+      
+        Parameters
+        ----------
+        root : tkinter
+            Tkinter GUI for searching parameters and creating posts
+      
+        """
+        
+        # Statics
+        self.n_widget = 0
+        n_frame = 0
+        
+        # Constants
+        WIDTH_ENTRY = 100
+        
+        # Root config
+        root.title("Articles publisher")
+        root.columnconfigure(0, weight = 1)
+        root.rowconfigure(0, weight = 1)      
+        
+        # Creating Frames
+        mainframe = ttk.Frame(root, padding = "3 3 12 12")
+        mainframe.grid(column = 0, row = n_frame, sticky = (N, W, E, S))
+        n_frame += 1
+        
+        self.urlframe = ttk.Frame(root, padding = "3 3 12 12")
+        self.urlframe.grid(column = 0, row = n_frame, sticky = (N, W, E, S))
+        n_frame += 1
+
+        self.resultframe = ttk.Frame(root, padding = "3 3 12 12")
+        self.resultframe.grid(column = 0, row = n_frame, sticky = (N, W, E, S))
+        n_frame += 1
+        
+        # Entry: Maximum articles to find 
+        self.maxi = StringVar() 
+        self.n_widget += 1
+        maxi_b = Button(mainframe, text = "Maximum number", disabledforeground = "black")
+        maxi_b["state"]="disable"
+        maxi_b.grid(column = 1, row = self.n_widget, sticky = (W, E))
+        ToolTip(maxi_b, msg="Maximum number of articles to search for")
+        maxi_entry = ttk.Entry(mainframe, width = WIDTH_ENTRY, textvariable=self.maxi)
+        maxi_entry.grid(column = 2, row = self.n_widget, sticky = (W, E))
+        
+        # Entry: Articles PMID to find
+        self.pmid = StringVar() 
+        self.n_widget += 1
+        maxi_b = Button(mainframe, text = "PMIDs", disabledforeground = "black")
+        maxi_b["state"]="disable"
+        maxi_b.grid(column = 1, row = self.n_widget, sticky = (W, E))
+        ToolTip(maxi_b, msg = "List of article PMIDs on Pubmed")
+        pmid_entry = ttk.Entry(mainframe, width = WIDTH_ENTRY, textvariable = self.pmid)
+        pmid_entry.grid(column = 2, row = self.n_widget, sticky = (W, E))
+        
+        # Entry: Date before publishing
+        self.date = StringVar() 
+        self.n_widget += 1
+        maxi_b = Button(mainframe, text = "Days", disabledforeground = "black")
+        maxi_b["state"]="disable"
+        maxi_b.grid(column = 1, row = self.n_widget, sticky = (W, E))
+        ToolTip(maxi_b, msg = "Maximum number of days since publishing date")
+        date_entry = ttk.Entry(mainframe, width = WIDTH_ENTRY, textvariable = self.date)
+        date_entry.grid(column = 2, row = self.n_widget, sticky = (W, E))
+
+        # Entry: Key terms
+        self.terms = StringVar() 
+        self.n_widget += 1
+        maxi_b = Button(mainframe, text = "Terms", disabledforeground = "black")
+        maxi_b["state"]="disable"
+        maxi_b.grid(column = 1, row = self.n_widget, sticky = (W, E))
+        ToolTip(maxi_b, msg = "Special terms like Bouix[Author]")
+        terms_entry = ttk.Entry(mainframe, width = WIDTH_ENTRY, textvariable = self.terms)
+        terms_entry.grid(column = 2, row = self.n_widget, sticky = (W, E))
+        
+        # Button: Search 
+        self.n_widget += 1
+        ttk.Button(mainframe, text = "Search", command = self.toSearch).grid(column = 1, row = self.n_widget, sticky = W)
+        root.bind("<Return>", self.toSearch)
+        
+        # Button: Publish
+        self.button_publish = ttk.Button(mainframe, text='Publish', command=self.toCheckButtons)
+        self.button_publish.grid(column = 2, row = self.n_widget, sticky = W)
+        root.bind("<Return>", self.toCheckButtons)
+        self.button_publish['state'] = 'disabled'
+        
+        # Some Polish
+        for child in mainframe.winfo_children(): 
+            child.grid_configure(padx = 10, pady = 10)
+        
+        # Default widget selection
+        maxi_entry.focus()
+
+    # Methods
+    def toSearch(self, *args):  
+        self.toResetFrames()
+        self.toFurnishFrames()
+        
+        url_search = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed"
+        if self.maxi.get() :
+          url_search += "&retmax=" + self.maxi.get()
+        if self.date.get() :
+          url_search += "&reldate=" + self.date.get()
+        if self.terms.get() :
+          url_search += "&term=" + self.terms.get()
+        url_search += "&usehistory=y"
+
+        page = requests.get(url_search)
+        soup = bs(page.content, "xml")
+        
+        self.label_search.grid()
+        self.searched_url.grid()
+        self.searched_url.insert(END, url_search)
+        self.searched_url['state'] = 'disabled'
+        
+        article_found = False
+        
+        try:
+            soup.find("eSearchResult").text
+            try:
+                error = soup.find("ERROR").text
+                self.stringvar_fetch.set(error)
+                self.label_fetch.grid()
+            except AttributeError:
+                try:
+                    message = soup.find("OutputMessage").text
+                    self.stringvar_fetch.set(message)
+                    self.label_fetch.grid()
+                except AttributeError:
+                    self.qk = soup.find("QueryKey").text
+                    self.we = soup.find("WebEnv").text
+                    article_found = True
+                    pass
+                pass
+        except AttributeError:
+            self.stringvar_fetch.set("Badly worded entry")
+            self.label_fetch.grid()
+            pass
+          
+        if article_found:
+            self.button_publish['state'] = 'normal'
+            self.toFetch()
+            
+    
+    def toResetFrames(self, *args):
+        self.button_publish['state'] = 'disabled'
+        
+        for widget in self.urlframe.winfo_children():
+            widget.destroy()
+            self.n_widget -= 1
+      
+        for widget in self.resultframe.winfo_children():
+            widget.destroy()
+            self.n_widget -= 1
+            
+    def toFurnishFrames(self, *args):
+        # Search URL: Use esearch command frome API Entrez
+        self.n_widget += 1
+        self.label_search=ttk.Label(self.urlframe, text = "Search URL:")
+        self.label_search.grid(column = 1, row = self.n_widget, sticky = (N, W, E, S))
+        self.label_search.grid_remove()
+        
+        self.n_widget += 1
+        self.searched_url=Text(self.urlframe, height = 1, width = 120)
+        self.searched_url.configure(font = ("Arial", 8))
+        self.searched_url.grid(column = 1, row = self.n_widget, sticky = (N, W, E, S))
+        self.searched_url.grid_remove()
+
+        # Fetch URL: Use efetch command frome API Entrez
+        self.stringvar_fetch = StringVar() 
+        self.n_widget += 1
+        self.label_fetch=ttk.Label(self.urlframe, textvariable = self.stringvar_fetch)
+        self.label_fetch.grid(column = 1, row = self.n_widget, sticky = (N, W, E, S))
+        self.label_fetch.grid_remove()
+        
+        self.n_widget += 1
+        self.fetched_url=Text(self.urlframe, height = 1, width = 120)
+        self.fetched_url.configure(font = ("Arial", 8))
+        self.fetched_url.grid(column = 1, row = self.n_widget, sticky = (N, W, E, S))
+        self.fetched_url.grid_remove()
+      
+    def toFetch(self, *args):
+        url_fetch = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&usehistory=y&query_key="+self.qk+"&WebEnv="+self.we #&rettype=medline&retmode=text
+        page = requests.get(url_fetch) 
+        self.soup = bs(page.content, "xml")
+        
+        self.stringvar_fetch.set("Fetch URL:")
+        self.label_fetch.grid()
+        self.fetched_url.grid()
+        self.fetched_url.insert(END, url_fetch)
+        self.fetched_url['state'] = 'disabled'
+        
+        self.toSplit()
+        
+    def toSplit(self, *args):
+        list_articles = self.soup.find_all("PubmedArticle")
+        
+        self.published_warning = StringVar() 
+        self.n_widget += 1
+        ttk.Label(self.resultframe, textvariable = self.published_warning).grid(column = 1, row = self.n_widget, sticky = (N, W, E, S))
+        
+        self.button_dict = {}
+        self.article_dict = {}
+        for i in range(len(list_articles)):
+            self.article_dict[str(i)] = Article(list_articles[i]) # class Article above
+            t=self.article_dict[str(i)].toGetTitle() + ", " + self.article_dict[str(i)].toGetDate() + ", " + self.article_dict[str(i)].toGetAuthor()
+            
+            self.button_dict[str(i)] = IntVar()
+            c = Checkbutton(self.resultframe, text=t, variable=self.button_dict[str(i)], onvalue=1, offvalue=0)
+            self.n_widget += 1
+            c.grid(column = 1, row = self.n_widget, sticky = W)
+        
+    def toCheckButtons(self, *args):
+        try:
+            for key, value in self.button_dict.items():
+                if value.get():
+                    self.article_dict[key].toPublish()
+                    self.published_warning.set("Published !")
+                    """
+                    xml_text = indent(str(self.article_dict[key].toGetSoup()))
+                    print(xml_text)
+                    """
+        except AttributeError:
+            pass
+#----------------------
+
+
 root = Tk()
 ArticlesFinder(root)
 root.mainloop()
